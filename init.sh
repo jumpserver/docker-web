66ba6fdc3a76d6f48736dda8e8bac727262fa462
ibuler <ibuler@qq.com>
2026-07-16 22:34:07 +0800

    perf: update redirect

--- init.sh
+++ init.sh
@@ -72,6 +72,7 @@ function config_https() {
   config_nginx "${config_file}"
 
   sed -i "s@server web:.*;@server localhost:51980;@g" "${config_file}"
+  sed -i 's@https://$server_name$request_uri;@https://$host$request_uri;@g' "${config_file}"
 
   if [ "${USE_IPV6}" == "1" ]; then
     sed -i "s@# listen \[::\]:443@listen \[::\]:443@g" "${config_file}"
